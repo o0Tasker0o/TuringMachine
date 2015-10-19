0f8aacf51bed975b691e71feee2d5d060967a9d2
James Tasker <james_tasker1@hotmail.com>
2015-10-19 16:13:15 +0100

    Add instruction class
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ TuringMachine/Instruction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TuringMachine
+{
+    public enum MoveDirection
+    {
+        Left,
+        Right,
+        None
+    }
+
+    public class Instruction
+    {
+        public Instruction()
+        {
+            MoveDirection = MoveDirection.None;
+        }
+
+        public String State
+        {
+            get;
+            set;
+        }
+
+        public char? ReadSymbol
+        {
+            get;
+            set;
+        }
+
+        public char? WriteSymbol
+        {
+            get;
+            set;
+        }
+
+        public MoveDirection MoveDirection
+        {
+            get;
+            set;
+        }
+
+        public String NextState
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ TuringMachineTests/InstructionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TuringMachine;
+
+namespace TuringMachineTests
+{
+    [TestClass]
+    public class InstructionTests
+    {
+        [TestMethod]
+        public void InstructionIsConstructedWithDefaultValues()
+        {
+            Instruction instruction = new Instruction();
+            Assert.AreEqual(null, instruction.State);
+            Assert.AreEqual(null, instruction.NextState);
+            Assert.AreEqual(MoveDirection.None, instruction.MoveDirection);
+            Assert.AreEqual(null, instruction.ReadSymbol);
+            Assert.AreEqual(null, instruction.WriteSymbol);
+        }
+    }
+}
