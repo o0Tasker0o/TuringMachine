1e05f9f9ce12b7d792027bac713a52ac80617416
James Tasker <james_tasker1@hotmail.com>
2015-10-19 16:18:56 +0100

    Add instruction table
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ TuringMachine/InstructionTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TuringMachine
+{
+    public class InstructionTable
+    {
+        private Dictionary<String, Instruction> mInstruction;
+
+        public InstructionTable()
+        {
+            mInstruction = new Dictionary<String, Instruction>();
+        }
+
+        public void AddInstruction(Instruction instruction)
+        {
+            if(null == instruction)
+            {
+                throw new ArgumentNullException("Instruction must not be null");
+            }
+
+            if(mInstruction.ContainsKey(instruction.State))
+            {
+                throw new ArgumentException("Instruction table already contains an instruction with this state name");
+            }
+            else
+            {
+                mInstruction.Add(instruction.State, instruction);
+            }
+        }
+
+        public Instruction GetInstruction(String instructionState)
+        {
+            return mInstruction[instructionState];
+        }
+    }
+}
---
+++ TuringMachineTests/InstructionTableTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TuringMachine;
+
+namespace TuringMachineTests
+{
+    [TestClass]
+    public class InstructionTableTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InstructionTableThrowsExceptionOnNullInstructions()
+        {
+            InstructionTable table = new InstructionTable();
+            table.AddInstruction(null);
+        }
+
+        [TestMethod]
+        public void InstructionTableStoresAddedInstructions()
+        {
+            InstructionTable table = new InstructionTable();
+
+            Instruction instructionA = new Instruction();
+            instructionA.State = "A";
+
+            table.AddInstruction(instructionA);
+
+            Assert.AreEqual(instructionA, table.GetInstruction(instructionA.State));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InstructionTableThrowsExceptionIf2InstructionsHaveTheSameState()
+        {
+            InstructionTable table = new InstructionTable();
+
+            Instruction instructionA = new Instruction();
+            instructionA.State = "A";
+
+            table.AddInstruction(instructionA);
+            table.AddInstruction(instructionA);
+        }
+    }
+}
