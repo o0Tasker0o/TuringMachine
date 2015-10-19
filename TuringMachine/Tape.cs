2981c243b1d3276980cfb11c96b6900259ff1418
James Tasker <james_tasker1@hotmail.com>
2015-10-19 15:27:09 +0100

    Add R/W tape
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ TuringMachine/Tape.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace TuringMachine
+{
+    public class Tape
+    {
+        private List<char?> mSymbols;
+        private int mIndex;
+
+        public Tape()
+        {
+            mSymbols = new List<char?>();
+            mSymbols.Add(null);
+            mIndex = 0;
+        }
+
+        public char? Read()
+        {
+            return mSymbols[mIndex];
+        }
+
+        public void Write(char? symbol)
+        {
+            mSymbols[mIndex] = symbol;
+        }
+
+        public void MoveLeft()
+        {
+            if(mIndex == 0)
+            {
+                mSymbols.Insert(0, null);
+            }
+            else
+            {
+                --mIndex;
+            }
+        }
+
+        public void MoveRight()
+        {
+            mSymbols.Add(null);
+            ++mIndex;
+        }
+    }
+}
---
+++ TuringMachineTests/TapeTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TuringMachine;
+
+namespace TuringMachineTests
+{
+    [TestClass]
+    public class TapeTests
+    {
+        [TestMethod]
+        public void ReadingNewTapeReturnsNull()
+        {
+            Tape tape = new Tape();
+
+            Assert.IsNull(tape.Read());
+        }
+
+        [TestMethod]
+        public void ReadReturnsLastWrittenCharacter()
+        {
+            Tape tape = new Tape();
+
+            char symbol = 'a';
+            tape.Write(symbol);
+
+            Assert.AreEqual(symbol, tape.Read());
+        }
+
+        [TestMethod]
+        public void MovingTapeLeftBringsBlankCellIntoView()
+        {
+            Tape tape = new Tape();
+
+            tape.Write('a');
+            tape.MoveLeft();
+
+            Assert.IsNull(tape.Read());
+        }
+
+        [TestMethod]
+        public void MovingTapeRightBringsBlankCellIntoView()
+        {
+            Tape tape = new Tape();
+
+            tape.Write('a');
+            tape.MoveRight();
+
+            Assert.IsNull(tape.Read());
+        }
+
+        [TestMethod]
+        public void MovingTapeLeftThenRightReturnsTapeToOriginalPosition()
+        {
+            Tape tape = new Tape();
+
+            char symbol = 'a';
+
+            tape.Write(symbol);
+            tape.MoveLeft();
+            tape.MoveRight();
+
+            Assert.AreEqual(symbol, tape.Read());
+        }
+
+        [TestMethod]
+        public void MovingTapeRightThenLeftReturnsTapeToOriginalPosition()
+        {
+            Tape tape = new Tape();
+
+            char symbol = 'a';
+
+            tape.Write(symbol);
+            tape.MoveRight();
+            tape.MoveLeft();
+
+            Assert.AreEqual(symbol, tape.Read());
+        }
+
+        [TestMethod]
+        public void TapeStoresAllWrittenSymbolsAsItMovesRight()
+        {
+            Tape tape = new Tape();
+
+            List<char> symbols = new List<char>() { 'a', 'b', 'c', 'd' };
+
+            for(int symbolIndex = 0; symbolIndex < symbols.Count; ++symbolIndex)
+            {
+                tape.Write(symbols[symbolIndex]);
+                tape.MoveRight();
+            }
+
+            for (int symbolIndex = symbols.Count - 1; symbolIndex >= 0; --symbolIndex)
+            {
+                tape.MoveLeft();
+                Assert.AreEqual(symbols[symbolIndex], tape.Read());
+            }
+        }
+
+        [TestMethod]
+        public void TapeStoresAllWrittenSymbolsAsItMovesLeft()
+        {
+            Tape tape = new Tape();
+
+            List<char> symbols = new List<char>() { 'a', 'b', 'c', 'd' };
+
+            for (int symbolIndex = 0; symbolIndex < symbols.Count; ++symbolIndex)
+            {
+                tape.Write(symbols[symbolIndex]);
+                tape.MoveLeft();
+            }
+
+            for (int symbolIndex = symbols.Count - 1; symbolIndex >= 0; --symbolIndex)
+            {
+                tape.MoveRight();
+                Assert.AreEqual(symbols[symbolIndex], tape.Read());
+            }
+        }
+    }
+}
