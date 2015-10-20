16cc094ea0b58264d47dc0a7f14cd1f18fe50514
James Tasker <james_tasker1@hotmail.com>
2015-10-20 08:52:37 +0100

    Add instruction parser
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ TuringMachine/InstructionParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TuringMachine
+{
+    public class InstructionParser
+    {
+        public static Instruction Parse(String instruction)
+        {
+            if(string.IsNullOrEmpty(instruction))
+            {
+                throw new ArgumentNullException("Instruction string must not be null");
+            }
+
+            string [] elements = instruction.Split(',');
+
+            if(elements.Length != 5)
+            {
+                throw new ArgumentException("Incorrect number of parameters specified for instruction");
+            }
+
+            Instruction parsedInstruction = new Instruction();
+            parsedInstruction.State = elements[0];
+            parsedInstruction.ReadSymbol = ParseSymbol(elements[1]);
+            parsedInstruction.WriteSymbol = ParseSymbol(elements[2]);
+            parsedInstruction.MoveDirection = (MoveDirection) Enum.Parse(typeof(MoveDirection), elements[3]);
+            parsedInstruction.NextState = elements[4];
+
+            return parsedInstruction;
+        }
+
+        private static char? ParseSymbol(string symbolString)
+        {
+            if (string.IsNullOrEmpty(symbolString))
+            {
+                return null;
+            }
+            else
+            {
+                return symbolString[0];
+            }
+        }
+    }
+}
---
+++ TuringMachineTests/InstructionParserTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TuringMachine;
+
+namespace TuringMachineTests
+{
+    [TestClass]
+    public class InstructionParserTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InstructionParserThrowsExceptionOnNullString()
+        {
+            Assert.IsNull(InstructionParser.Parse(null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InstructionParserThrowsExceptionOnEmptyString()
+        {
+            Assert.IsNull(InstructionParser.Parse(""));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InstructionParserThrowsExceptionOnTooFewArguments()
+        {
+            Assert.IsNull(InstructionParser.Parse("START,a,b,Right"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InstructionParserThrowsExceptionOnTooManyArguments()
+        {
+            Assert.IsNull(InstructionParser.Parse("START,a,b,Right,HALT,Extra Info"));
+        }
+
+        [TestMethod]
+        public void InstructionParserParsesValidInstruction()
+        {
+            Instruction parsedInstruction = InstructionParser.Parse("START,a,b,Right,HALT");
+            Assert.AreEqual("START", parsedInstruction.State);
+            Assert.AreEqual('a', parsedInstruction.ReadSymbol);
+            Assert.AreEqual('b', parsedInstruction.WriteSymbol);
+            Assert.AreEqual(MoveDirection.Right, parsedInstruction.MoveDirection);
+            Assert.AreEqual("HALT", parsedInstruction.NextState);
+        }
+
+        [TestMethod]
+        public void InstructionParserTreatsBlankSymbolsAsNull()
+        {
+            Instruction parsedInstruction = InstructionParser.Parse("START,,,Right,HALT");
+            Assert.IsNull(parsedInstruction.ReadSymbol);
+            Assert.IsNull(parsedInstruction.WriteSymbol);
+        }
+    }
+}
