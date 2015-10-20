f3263e699e1c6516f3494795a0c92f454cf3762d
James Tasker <james_tasker1@hotmail.com>
2015-10-20 14:47:48 +0100

    Load clock delay from configuration file
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- TuringMachine/Program.cs
+++ TuringMachine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -31,7 +32,8 @@ namespace TuringMachine
                     break;
                 }
 
-                Thread.Sleep(400);
+                int clockDelay = Int32.Parse(ConfigurationManager.AppSettings.Get("ClockDelay"));
+                Thread.Sleep(clockDelay);
             }
 
             Console.WriteLine("Execution completed!");
